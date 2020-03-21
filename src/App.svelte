ac05bcdd900e546779eeeff5ea7bb6363a1ecd82
Aaron <abarnard@protonmail.com>
2020-03-21 16:47:35 +1100

    Initial page

---
+++ src/App.svelte
@@ -0,0 +1,138 @@
+<script>
+  import tracks from "./content";
+</script>
+
+<style>
+  @font-face {
+    font-family: "Dank Mono";
+    src: url("/dankmono.otf");
+  }
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: "Dank Mono";
+    box-sizing: border-box;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  header,
+  footer {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+  }
+
+  h2 {
+    color: #282828;
+    margin: 1rem;
+    text-transform: uppercase;
+  }
+
+  h3 {
+    margin: 0;
+  }
+
+  section {
+    display: flex;
+  }
+
+  div.description {
+    padding: 0 1rem;
+    width: 40%;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  ul.tracks {
+    height: 88vh;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: linear-gradient(
+      /* #ff71ce,
+      #e9eaeb, */ #05ffa1,
+      #b967ff /* #01cdfe, */ /* #fffb96, */ /* #ff0563 */
+    );
+    margin: 0 0.5rem;
+    padding: 0 2rem;
+    overflow: scroll;
+  }
+
+  ul.tracks li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    padding: 1rem;
+    background: white;
+  }
+
+  div.musicians {
+    display: flex;
+    margin: 0.5rem 0;
+  }
+
+  div.musicians ul {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0 1rem;
+  }
+</style>
+
+<main>
+  <header>
+    <h2>Songs from Isolation</h2>
+  </header>
+  <section>
+    <div class="description">
+      <p>
+        There are many bored musicians at home right now due to the unprecented
+        COVID-19 spread...
+      </p>
+      <p>
+        Seems like a unique opportunity to collaborate digitally, across borders
+        and release a
+        <b>new piece of music every day...</b>
+      </p>
+      <p>
+        If you are a creative and would like to participate in any way, let's
+        collab!
+      </p>
+      <p>shoot us an email: songsfromisolation@gmail.com</p>
+    </div>
+    <ul class="tracks">
+      {#each tracks as { date, name, description, embed, musicians }}
+        <li>
+          <h3>{date} - {name}</h3>
+          <p>{description}</p>
+          <div class="musicians">
+            <span>Musicians:</span>
+            <ul>
+              {#each musicians as musician}
+                <a
+                  href={musician.link}
+                  target="_blank"
+                  rel="noreferrer noopener">
+                  {musician.name}
+                </a>
+              {/each}
+            </ul>
+          </div>
+          {@html embed}
+        </li>
+      {/each}
+    </ul>
+  </section>
+</main>
---
+++ src/content.js
@@ -0,0 +1,48 @@
+const colors = [
+  "e9eaeb",
+  "05ffa1",
+  "ff71ce",
+  "b967ff",
+  "01cdfe",
+  "fffb96",
+  "ff0563"
+];
+
+const tracks = [
+  {
+    name: "Track Name",
+    date: "03/21/2020",
+    description: "This track is about something",
+    musicians: [
+      { name: "John Gribbin", link: getTwitterLink("johngribbo") },
+      { name: "Aaron Barnard", link: getTwitterLink("tarrendarnard") }
+    ],
+    embed: createIframe("744667294", getRandomColor())
+  },
+  {
+    name: "Track Name",
+    date: "03/22/2020",
+    description: "This track is about something else",
+    musicians: [
+      { name: "John Gribbin", link: getTwitterLink("johngribbo") },
+      { name: "Aaron Barnard", link: getTwitterLink("tarrendarnard") }
+    ],
+    embed: createIframe("322167648", getRandomColor())
+  }
+];
+
+function createIframe(track, color) {
+  return `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${track}&color=%23${color}&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * (colors.length - 1 - 1));
+  console.log({ index });
+  return colors[index];
+}
+
+function getTwitterLink(user) {
+  return `https://twitter.com/${user}`;
+}
+
+export default tracks;
